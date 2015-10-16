f7107686f0aef81d1691ef1984815ae5a2790306
Derek Hopper <hopper.derek@gmail.com>
2015-10-16 11:23:28 -0400

    Gives us a nice version number.

--- lib/garbage_man/version_number.rb
+++ lib/garbage_man/version_number.rb
@@ -1,9 +1,9 @@
 module GarbageMan
   module VERSION
     MAJOR = 0
-    MINOR = 0
-    TINY  = 1
-    PRE   = "pre"
+    MINOR = 1
+    TINY  = 0
+    PRE   = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join('.')
 
